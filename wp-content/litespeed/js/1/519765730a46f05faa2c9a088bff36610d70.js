bf8ec6b6a05bcca0d5508086e90660615d36fe3e
ItsShivamGupta <shivam.moneymoves@gmail.com>
2025-11-15 16:49:43 +0530

    Add files via upload

---
+++ wp-content/litespeed/js/1/519765730a46f05faa2c9a088bff36610d70.js
@@ -0,0 +1,2 @@
+(function($){'use strict';var WidgetCounterBlockHandler=function($scope){$('.elementor-section').on('mouseover','.counter-block-two',function(){$('.counter-block-two.item-active').removeClass('item-active');$(this).addClass('item-active')})};$(window).on("elementor/frontend/init",function(){elementorFrontend.hooks.addAction("frontend/element_ready/tm-ele-counter-block.skin-style2",WidgetCounterBlockHandler)})})(jQuery)
+;
